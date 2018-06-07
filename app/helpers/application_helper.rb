3d11bcdfe9117d085075271f4c69ab163ca4ab52
Josh Cunningham <josh@joshcanhelp.com>
2018-06-07 12:36:28 -0700

    meta title partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Returns the full title per page
+  def meta_title (page_title = '')
+    base_title = 'Basic Auth0 Ruby App'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,13 +10,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_path
     assert_response :success
-    assert_select "title", "Home#{@title_sep}#{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home#{@title_sep}#{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
